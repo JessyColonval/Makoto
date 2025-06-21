c1b71d2ca7b0b499375efc98467bcc68b1efdfee
JessyColonval <jessy.colonval@gmail.com>
2025-06-21 17:54:01 +0200

    Comments in test_absolute

--- tests/mathematics/test_absolute.py
+++ tests/mathematics/test_absolute.py
@@ -11,40 +11,64 @@ from src.mathematics.method_distance import Absolute
 
 
 class TestAbsolute(TestCase):
+    """
+    Unit tests of functions that calculate an absolute distance between two
+    points.
+    """
 
     @staticmethod
-    def randomword(length):
+    def randomword(length: int):
+        """
+        Generates a random word of a given length.
+
+        Parameters
+        ----------
+        length: int
+            Desired word length.
+        """
         letters = string.printable
         return ''.join(random.choice(letters) for i in range(length))
 
-    def test_distance_nD_same_object_coords(self):
-        l_str = 10
-        for length in range(1, 1000):
+    def test_distance_same_object_coords(self):
+        """
+        Verifies if the distance calculation between two identical objects is
+        equal to zero.
+        """
+        length = 10
+        for n in range(1, 1000):
             coords = np.array(
-                [TestAbsolute.randomword(l_str) for _ in range(0, length)],
+                [TestAbsolute.randomword(length) for _ in range(0, n)],
                 dtype=str
             )
-            result = Absolute.distance(coords, coords)
-            expected = 0
-            self.assertEqual(result, expected)
+            actual = Absolute.distance(coords, coords)
+            self.assertEqual(0, actual)
 
-    def test_distance_nD_example(self):
+    def test_distance_all_differents(self):
+        """
+        Verifies if the distance calculation is correct when all elements are
+        different in the two points.
+        """
         a = np.array(["bleu", "rouge", "noir"], dtype=str)
         b = np.array(["rose", "violet", "marron"], dtype=str)
-        result = Absolute.distance(a, b)
-        expected = 3
-        self.assertEqual(result, expected)
+        actual = Absolute.distance(a, b)
+        self.assertEqual(3, actual)
 
-    def test_distance_nD_equal_to_1(self):
+    def test_distance_one_different(self):
+        """
+        Verifies if the distance calculation is equal to one when only one
+        element is different in the two points.
+        """
         a = np.array(["bleu", "rouge", "noir"])
-        b = np.array(["bleu", "violet", "marron"])
-        result = Absolute.distance(a, b)
-        expected = 2
-        self.assertEqual(result, expected)
+        b = np.array(["bleu", "rouge", "marron"])
+        actual = Absolute.distance(a, b)
+        self.assertEqual(1, actual)
 
-    def test_distance_nD_equal_twice(self):
+    def test_distance_two_different(self):
+        """
+        Verifies if the distance calculation is equal to two when two elements
+        are differents in the two points.
+        """
         a = np.array(["bleu", "bleu", "noir"])
         b = np.array(["bleu", "violet", "marron"])
-        result = Absolute.distance(a, b)
-        expected = 2
-        self.assertEqual(result, expected)
+        actual = Absolute.distance(a, b)
+        self.assertEqual(2, actual)
