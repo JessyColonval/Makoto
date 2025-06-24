6920728eea1f40f142c75127fb957fb30ec5c5bf
JessyColonval <jessy.colonval@gmail.com>
2025-06-24 12:06:53 +0200

    Comments & Renames functions in point.py

--- src/modelisation/point.py
+++ src/modelisation/point.py
@@ -3,18 +3,18 @@ Written by Jessy Colonval.
 """
 from __future__ import annotations
 
-from math import sqrt
 from random import shuffle
 import numpy as np
 import numpy.typing as npt
+from src.mathematics.method_distance import Euclidean, Manhattan, Absolute
 
 
 class PointIterator():
     """
-    Iterator useful to iterate contextual values of a point.
+    Iterator useful to iterate contextual and behavioral values of a point.
     """
 
-    def __init__(self, point):
+    def __init__(self, point: Point):
         self.__point = point
         self.__index = 0
 
@@ -136,48 +136,192 @@ class Point():
         return True
 
     def copy(self) -> Point:
+        """
+        Copy the point into a new Point object.
+
+        Return
+        ------
+        Point
+            A copy of this point.
+        """
         return Point(
-            np.copy(self.__ctxs),
-            np.copy(self.__bhvs)
+            self.__ctxs,  # Already copied in the constructor.
+            self.__bhvs
         )
 
     def contextual(self, index: int) -> float:
+        """
+        Gets the contextual value at a given index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the wanted contextual value.
+
+        Return
+        ------
+        float
+            The contextual value at the given index.
+
+        Raises
+        ------
+        IndexError
+            when the index is negative or greater or equal to the number of
+            contextual values.
+        """
         if index < 0 or index >= len(self.__ctxs):
             raise IndexError("Out of range.")
         return self.__ctxs[index]
 
-    def contextuals(self):
+    def contextuals(self) -> npt.NDArray[np.float64]:
+        """
+        Gets a copy of the array that contains all the contextual values.
+
+        Return
+        ------
+        NDArray[float64]
+            a copy of the numpy that contains all contextual values.
+        """
         return self.__ctxs.copy()
 
     def number_contextuals(self) -> int:
+        """
+        Gets the number of contextual values.
+
+        Return
+        ------
+        int
+            The number of contextual values.
+        """
         return len(self.__ctxs)
 
-    def set_contextual(self, i_ctx, const) -> None:
-        self.__ctxs[i_ctx] = const
+    def set_contextual(self, i_ctx: int, value: np.float64) -> None:
+        """
+        Sets the contextual value at the given index with the given value.
+
+        Parameters
+        ----------
+        i_ctx : int
+            The index of the contextual value that will be modify.
+        value: float64
+            Its new contextual value.
+        """
+        self.__ctxs[i_ctx] = value
 
     def behavior(self, index: int) -> int:
+        """
+        Gets the behavioral value at a given index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the wanted behavioral value.
+
+        Return
+        ------
+        float
+            The behavioral value at the given index.
+
+        Raises
+        ------
+        IndexError
+            when the index is negative or greater or equal to the number of
+            contextual values.
+        """
         if index < 0 or index >= len(self.__bhvs):
             raise IndexError("Out of range.")
         return self.__bhvs[index]
 
-    def behaviors(self):
+    def behaviors(self) -> npt.NDArray[np.float64]:
+        """
+        Gets a copy of the array that contains all the behavioral values.
+
+        Return
+        ------
+        NDArray[float64]
+            a copy of the numpy that contains all contextual values.
+        """
         return self.__bhvs.copy()
 
     def number_behaviors(self) -> int:
+        """
+        Gets the number of behavioral values.
+
+        Return
+        ------
+        int
+            The number of behavioral values.
+        """
         return len(self.__bhvs)
 
-    def distance(self, other: Point) -> float:
-        if self.number_contextuals() != other.number_contextuals():
-            raise ValueError("Points must have same dimension.")
-        _sum = 0.0
-        for i in range(0, self.number_contextuals()):
-            _sum += (self.contextual(i) - other.contextual(i))**2
-        return sqrt(_sum)
+    def set_behavior(self, i_bhv: int, value: np.int16) -> None:
+        """
+        Sets the behavioral value at the given index with the given value.
 
-    def shuffle_contextuals(self, indices: np.ndarray = None) -> None:
+        Parameters
+        ----------
+        i_bhv : int
+            The index of the behavioral value that will be modify.
+        value: int16
+            Its new behavioral value.
+        """
+        self.__bhvs[i_bhv] = value
+
+    def distance(self, other: Point, method: str = "euclidean") -> float:
+        """
+        Computes the distance between two points.
+
+        Parameters
+        ----------
+        other : Point
+            An another point where its distance with this point will be
+            calculated.
+        method : str, optional
+            The method of distance used between 'euclidean', 'manhattan' and
+            'absolute'.
+            Default is 'euclidean'.
+
+        Return
+        ------
+        float
+            The distance between these two points according the method used.
+
+        Raises
+        ------
+        ValueError
+            when an unknow method of distance is given.
+        """
+        if method == "euclidean":
+            return Euclidean.distance(self.__ctxs, other.contextuals())
+        if method == "manhattan":
+            return Manhattan.distance(self.__ctxs, other.contextuals())
+        if method == "absolute":
+            return Absolute.distance(self.__ctxs, other.contextuals())
+        raise ValueError("Unknow method of distance computation.")
+
+    def shuffle_contextuals(self, indices: npt.NDArray[np.int16] = None
+                            ) -> None:
+        """
+        Shuffles the contextual values.
+
+        Parameters
+        ----------
+        indices : NDArray[int16], optional
+            The pairs of indices to which each value corresponds will be
+            interchanged.
+
+        Raises
+        ------
+        ValueError
+            when one of the indices are negative or greater or equal to the
+            number of contextual values.
+        """
+        # Without indices, all contextual values are randomly shuffle.
         if indices is None:
             shuffle(self.__ctxs)
+
         else:
+            # Checks if all indices are between 0 and the contextuals' size.
             if any(i < 0 or j < 0
                    or i >= len(self.__ctxs)
                    or j >= len(self.__ctxs)
@@ -185,12 +329,32 @@ class Point():
                 raise ValueError("Indices must be between 0 and the number ",
                                  "of contextual attributes.")
 
+            # Swaps values between each indices.
             for i, j in indices:
                 self.__ctxs[i], self.__ctxs[j] = self.__ctxs[j], self.__ctxs[i]
 
-    def shuffle_behaviors(self, indices: np.ndarray = None) -> None:
+    def shuffle_behaviors(self, indices: npt.NDArray[np.int16] = None
+                          ) -> None:
+        """
+        Shuffles the behavioral values.
+
+        Parameters
+        ----------
+        indices : NDArray[int16], optional
+            The pairs of indices to which each value corresponds will be
+            interchanged.
+
+        Raises
+        ------
+        ValueError
+            when one of the indices are negative or greater or equal to the
+            number of behavioral values.
+        """
+        # Without indices, all behavioral values are randomly shuffle.
         if indices is None:
             shuffle(self.__bhvs)
+
+        # Checks if all indices are between 0 and the contextuals' size.
         else:
             if any(i < 0 or j < 0
                    or i >= len(self.__bhvs)
@@ -199,11 +363,17 @@ class Point():
                 raise ValueError("Indices must be between 0 and the number ",
                                  "of behavioral attributes.")
 
+        # Swaps values between each indices.
             for i, j in indices:
                 self.__bhvs[i], self.__bhvs[j] = self.__bhvs[j], self.__bhvs[i]
 
-    def change_behaviors(self, i_bhv, new_bhv) -> None:
-        self.__bhvs[i_bhv] = new_bhv
+    def to_numpy(self) -> npt.NDArray[np.float64]:
+        """
+        Converts the point into a single numpy array.
 
-    def to_numpy(self):
+        Return
+        ------
+        NDArray[float64]
+            The contextual and behavioral values of its point.
+        """
         return np.concatenate((self.__ctxs, self.__bhvs))
